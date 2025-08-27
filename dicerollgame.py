0c68723412982efa834a495cb60516da451bf67e
kali <kali@kali.kali>
2025-08-27 06:44:57 -0400

    Initial commit
    Committer: scriptz0340 <scriptz0340@gmail.com>

---
+++ dicerollgame.py
@@ -0,0 +1,20 @@
+import random
+
+while True: 
+    choice = input("Roll the dice? (y/n): ") 
+    if choice.lower() == 'y':
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f"({die1}, {die2})")
+    elif choice.lower() == 'n':
+        print("Thanks for playing!")
+        quit()
+    else:
+        print("Invalid choice!")
+        
+
+# The infinite loop allows the game to continue on forever until user decides to quit.
+# At first i thought to break the loop if 'y' but that would cause the program to terminate after one roll instead of allowing user to keep playing.
+# I also learned about the 'random' module in python, i didnt know that existed so i couldnt figure out on my own how to generate the random numbers.
+
+# I was able to do everything except generate the random numbers on my own.
